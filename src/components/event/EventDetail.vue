eca8460a9cb10ee627fb20c439cfe1994b06025f
musicsoul3388 <117941774+musicsoul3388@users.noreply.github.com>
2022-12-08 15:08:36 +0000

    Karten erstellt

--- src/components/event/EventDetail.vue
+++ src/components/event/EventDetail.vue
@@ -1,8 +1,40 @@
-
 <template>
   <div>
     Event Details und Beschreibung für:
     {{ event?.Titel }}
+    <b-card overlay
+    img-src="https://picsum.photos/900/250/?image=3"
+    img-alt="Card Image"
+    text-variant="white"
+    title="Fußball"
+    sub-title="Uhrzeit?">
+    <b-card-text>
+      Infos über das Spiel
+    </b-card-text>
+  </b-card>
+</br>
+  <b-card overlay
+    img-src="https://picsum.photos/900/250/?image=3"
+    img-alt="Card Image"
+    text-variant="white"
+    title="Handball"
+    sub-title="Uhrzeit?">
+    <b-card-text>
+      Infos über das Spiel
+    </b-card-text>
+  </b-card>
+</br>
+  <b-card overlay
+    img-src="https://picsum.photos/900/250/?image=3"
+    img-alt="Card Image"
+    text-variant="white"
+    title="Turnen"
+    sub-title="Uhrzeit?">
+    <b-card-text>
+      Infos über das Spiel
+    </b-card-text>
+  </b-card>
+</br>
   </div>
 </template>
 <script>
