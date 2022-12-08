52293fce89b7eea5705ed6c9be6badc29bc2220d
Stephan Strittmatter <stritti@users.noreply.github.com>
2022-12-08 15:15:45 +0000

    initial version of sportartList

---
+++ src/components/sportart/SportartList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sportart-list"></div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'pinia'
+import { useSportartStore } from '../../stores/sportart'
+
+export default {
+    name: 'SportartList',
+  computed: {
+    ...mapState(useSportartStore, ['eventList'])
+  },
+  methods: {
+    ...mapActions(useSportartStore, ['loadEvents'])
+  }
+}
+</script>
+
+<style>
+</style>
